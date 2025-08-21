f78d461d8ee90df050364b92ca176f6f99b80c9a
Dimas-Eko <dhimasprasetyo0@gmail.com>
2025-08-21 18:19:55 +0700

    configure for deploy to aws

--- server/app.js
+++ server/app.js
@@ -1,3 +1,10 @@
+if(process.env.NODE_ENV !== 'production'){
+    require("dotenv").config();
+}
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 const express = require('express')
 const app = express()
 const TripController = require ('./controllers/TripController')
